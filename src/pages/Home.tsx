5386d8de04beabf28a81a177b7b0dcb7f2dba21c
SimoneMorella <simonemorella21@gmail.com>
2024-09-08 20:13:40 +0200

    added neon anim and colors for home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,19 @@
 
 export default function Home () {
     return (
-        <div>
-            <h1 className="font-neon text-9xl">
-                <span>Neon</span>
-                <span className="font-cyberWay">Games</span>
+        <div className="min-h-svh bg-customBg flex justify-center items-center">
+            <div className="flex flex-col justify-center gap-32 p-8 -mt-32">
+                <h1 className="font-neon text-7xl p-4 flex items-center">
+                    <span className="neonLogo inline-block h-16 animate-textPulsate">Ne<span className="animate-textFlick">o</span>n</span>
+                    <span className="font-cyberWay text-white inline-block h-16">Games</span>
                 </h1>
+                <button className="font-neon text-white text-center text-4xl w-40 self-end py-4 px-8 pt-6 neonLogo neonBorder  animate-borderPulse">
+                    <div className="animate-textPulsate">
+                        S<span className="animate-textFlick">H</span>OP
+                    </div>
+                </button>
+            </div>
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,48 @@ export default {
       fontFamily: {
         neon: ['NeonNeon', 'sans-serif'],
         cyberWay: ['CyberWay', 'sans-serif'],
-      }
+      },
+      backgroundImage: {
+        customBg: 'linear-gradient(165deg, rgba(0,0,0,1) 0%, rgba(27,11,28,1) 70%, rgba(52,22,46,1) 100%);'
+      },
+      keyframes: {
+        textPulsate: {
+          "0%": {
+            textShadow: "0 0 4px #ffffff4d, 0 0 6px #ffffff4d, 0 0 20px #FA4B9C, 0 0 30px #FA4B9C, 0 0 40px #FA4B9C",
+          },
+          "100%": {
+            textShadow: "0 0 6px #ffffff4d, 0 0 10px #ffffff4d, 0 0 25px #FA4B9C, 0 0 38px #FA4B9C, 0 0 50px #FA4B9C, 0 0 58px #FA4B9C",
+          }
+        },
+        textFlick: {
+          "0%, 2%, 21%": {
+            opacity: 0.1,
+          },
+          "4%, 19%, 80%, 83%": {
+            opacity: 0.5,
+          },
+          "23%, 87%" : {
+            opacity: 1,
+          }
+        },
+        borderPulse: {
+          "0%, 4%": {
+            opacity: 0.1,
+          },
+          "2%, 8%, 100%": {
+            opacity: 1,
+          },
+          "70%": {
+            opacity: 0.7,
+          }
+
+        }
+      },
+      animation: {
+        textPulsate: 'textPulsate 2s infinite alternate',
+        textFlick: 'textFlick 2s linear infinite',
+        borderPulse: 'borderPulse 2s infinite linear',
+      },
     },
   },
   plugins: [],
