cab5b96eea780bba75cdf3f790a02f788e45a386
SimoneMorella <simonemorella21@gmail.com>
2024-09-25 23:19:22 +0200

    fixed animation bug with overflow

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -9,16 +9,16 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   const location = useLocation();
     return (
-      <div className="min-h-svh bg-customBg flex flex-col">
+      <div className="min-h-svh bg-customBg flex flex-col overflow-hidden">
         <Navbar />
         <AnimatePresence mode="wait">
           <motion.div
             key={location.pathname}
             className="flex flex-col flex-1"
-            initial={{ opacity: 0, x: "10px" }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ x: "10px", opacity: 0 }}
-            transition={{duration: 0.3}}
+            initial={{ opacity: 0, x: "30px" }}
+            animate={{ opacity: 1, x: 0}}
+            exit={{ x: "30px", opacity: 0 }}
+            transition={{duration: 0.5}}
             >
             {children}
           </motion.div>
